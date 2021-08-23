6bf595b5c75b861a6ba95dc1edcd3e193bcea755
Sebastian Sudra <sebastian.sudra@pentacomp.pl>
2021-08-23 18:16:34 +0200

    install angular material

--- CEN/src/app/shared/shared.module.ts
+++ CEN/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
     imports: [CommonModule],
-    exports: [CommonModule, FormsModule],
+    exports: [CommonModule, FormsModule, BrowserAnimationsModule],
     declarations: [],
 })
 export class SharedModule {}
